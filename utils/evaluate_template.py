1b8e5ea23b8cecd1ce0252d677dc42e30ce4c760
Jan Warchoł <jan.warchol@gmail.com>
2019-11-10 15:18:10 +0100

    Add two more monotones
    It seems they will be necessary to design a good monochromatic theme.

--- utils/evaluate_template.py
+++ utils/evaluate_template.py
@@ -20,7 +20,7 @@ DEFAULT_OUTPUT_DIR = ''
 MARKER_RE = re.compile(r'!!COL(?P<delim>.)(?P<format>.*?)(?P=delim)')
 
 DEFAULT_COLOR_ORDER = [
-    'bg', 'bg_bright_1', 'bg_bright_2', 'fg_dim', 'fg', 'fg_bright',
+    'bg', 'bg_bright_1', 'bg_bright_2', 'fg_dim', 'fg_dim_2', 'fg', 'fg_bright', 'fg_bright_2',
     'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'orange', 'violet',
     'br_red', 'br_green', 'br_yellow', 'br_blue', 'br_magenta', 'br_cyan', 'br_orange', 'br_violet'
 ]
--- utils/palettes/grey/grey-18-74-darkula.py
+++ utils/palettes/grey/grey-18-74-darkula.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import selenized_base
 
 # Variant based on fg and bg lightness of darkula palette from Jetbrains
@@ -7,5 +8,13 @@ palette = selenized_base.generate_palette(
     background=(18, 0, 0),
     # actual foreground shade is  74, -3, -9
     foreground=(74, 0, 0),
+    monotone_spec={
+        "bg_bright_1": [1/10,  1],
+        "bg_bright_2": [1/4,   1],
+        "fg_dim":      [2/4,   1],
+        "fg_dim_2":    [3/4,   1],
+        "fg_bright_2": [1+1/8, 1],
+        "fg_bright":   [1+1/4, 1],
+    },
 )
 
